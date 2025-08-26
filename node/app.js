513c9ec0414f19fa8ccfc6b8c49a534d09c803c0
sheri994 <sheritesta@gmail.com>
2025-08-26 01:09:56 -0700

    Update app.js

--- node/app.js
+++ node/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-// TODO: implement POST /api/sum
+app.post('/api/sum', (req, res) => {
+  const { a, b } = req.body;
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return res.status(400).json({ error: "Invalid input" });
+  }
+  res.json({ sum: a + b });
+});
 
 module.exports = app;
